b2769b18f08289e0d72ef15265faa8142ac95d5f
Rukaiya Amin <rukaiyaamin36@gmail.com>
2023-06-06 06:55:03 +0600

    25:12

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-  
-    </>
+    <BrowserRouter>
+    <Box sx={{backgroundColor: '#000'}}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTern' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
   )
 }
 
